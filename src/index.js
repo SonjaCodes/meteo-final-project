93bebe4adb4c686688d7bfe79c14721c84807fc0
SonjaCodes <sonja@kolmer.cc>
2024-04-09 23:22:22 +0200

    js index days below 5

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ function displayForecast(response) {
   let forecastHTML = "";
 
   response.data.daily.forEach(function (day, index) {
-    if (index > 6) {
+    if (index < 5) {
       forecastHTML =
         forecastHTML +
         `
