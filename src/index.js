db00b35c01d7b55e3409b4a2a1476587623c6af0
SonjaCodes <sonja@kolmer.cc>
2024-04-10 00:06:15 +0200

    updated index.js with current day

--- src/index.js
+++ src/index.js
@@ -57,6 +57,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
@@ -72,8 +79,8 @@ function displayForecast(response) {
         forecastHTML +
         `
     <div class="weather-forecast-day">
-      <div class="weather-forecast-date">Tue</div>
-        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}°</strong>
